357c85533485d6b0ee9f0f63400725f8efb6816c
Alejandro Beggiato <59038405+tiburonvaldez@users.noreply.github.com>
2023-02-24 19:53:37 -0300

    WildShop-App(Xamarin)

---
+++ WildShop/WildShop/MasterDetailPage/MasterDetail.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using WildShop.MasterDetailPage;
+using WildShop.Data;
+using WildShop.Models;
+
+
+namespace WildShop.MasterDetailPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MasterDetail : IMasterDetailPageController
+	{
+		public MasterDetail ()
+		{
+			InitializeComponent ();
+            this.Master = new Master();
+            this.Detail = new NavigationPage(new Detail());
+            App.MasterDet = this;
+
+		}
+	}
+}
